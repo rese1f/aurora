3bb923b215aa676407dddae748e184d9e870cf16
wenhao <reselfchai@gmail.com>
2024-09-23 05:59:50 +0000

    clean xtuner-lite

--- src/xtuner_lite/_lite/chat/templates/__init__.py
+++ src/xtuner_lite/_lite/chat/templates/__init__.py
@@ -7,7 +7,19 @@ CHAT_TEMPLATE_MAP = {
         system='<|im_start|>system\n{system}<|im_end|>\n',
         user='<|im_start|>user\n{user}<|im_end|>\n<|im_start|>assistant\n',
         assistant='{assistant}<|im_end|>',
-        stop_words=['<|im_end|>'])
+        stop_words=['<|im_end|>']
+    ),
+    'vicuna':
+    HybridChatTemplate(
+        system='A chat between a curious user and an artificial '
+                'intelligence assistant. The assistant gives '
+                'helpful, detailed, and polite answers to the '
+                'user\'s questions. {system}\n ',
+        user='USER: {input} ASSISTANT:',
+        # TODO: check with assistant and stop_words
+        assistant='{assistant}</s>',
+        stop_words=['</s>']
+    ),
 }
 
 __all__ = ['ChatTemplate', 'HybridChatTemplate']
